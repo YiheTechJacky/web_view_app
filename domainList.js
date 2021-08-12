9a231bd692126f10405de5da29867846ef206cd8
Jacky Lin <jacky@yihetech168.com>
2021-08-12 17:58:37 +0800

    update

--- domainList.js
+++ domainList.js
@@ -2,6 +2,9 @@ const domainList = {
     test: [
         'https://web-dev.yihetech168.com',
     ],
+    "11602test": [
+        'http://zhongo.okkoinc.com',
+    ],
 };
 
 export default domainList;
